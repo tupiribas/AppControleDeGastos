e2d042cddaab29fc2da2eb8fe01b7bbc1c735fe8
Tupi Guedes <tupiguedes@outlook.com>
2022-12-10 04:59:08 -0300

    Implementação visual (obj): Transaction - v2.3 - formatação do valor

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MyHomePage extends StatelessWidget {
                           border: Border.all(color: Colors.purple, width: 2)),
                       padding: const EdgeInsets.all(10),
                       child: Text(
-                        tr.value.toString(),
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
                         style: const TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
