4d2b10e7fb9fbde5f72f246b45f896d2c510e172
Tupi Guedes <tupiguedes@outlook.com>
2022-12-10 05:08:06 -0300

    Implementação visual (obj): Transaction - v2.4 - formatação da data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main(List<String> args) {
   runApp(const ExpensesApp());
@@ -78,7 +79,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),// '${tr.date.toString()}',
                           style: const TextStyle(color: Colors.grey),
                         )
                       ],
