f9ab7251266917f64fe60f7d030cef6ac63456ad
Tupi Guedes <tupiguedes@outlook.com>
2022-12-10 02:44:14 -0300

    ~~PRIMEIRA VERSÃO~~

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Versao Inicial!'),
+    );
+  }
+}
