87b1ea2d4765a369e58bd61d8d25d857f8352685
Tupi Guedes <tupiguedes@outlook.com>
2022-12-11 15:28:32 -0300

    Implementação visual: Tema de estilo - v1.0 - main

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,84 @@
-import 'dart:math';
-
-import 'package:expenses/components/transaction_form.dart';
-import 'package:expenses/models/transaction.dart';
+// ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'dart:math';
+import './components/transaction_form.dart';
+import './components/transaction_list.dart';
+import 'models/transaction.dart';
 
-import 'components/transaction_list.dart';
+main() => runApp(const Expenses());
 
-void main(List<String> args) {
-  runApp(const ExpensesApp());
-}
-
-class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({super.key});
+class Expenses extends StatelessWidget {
+  const Expenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    final ThemeData tema = ThemeData();
+
+    return MaterialApp(
+      home: const MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          headline6: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
   final _transactions = [
     Transaction(
-        id: 't1',
-        title: 'Novo Tenis de corrida',
-        value: 310.70,
-        date: DateTime.now()),
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
     Transaction(
-        id: 't2', title: 'Conta de luz', value: 151.15, date: DateTime.now()),
-    Transaction(
-        id: 't3', title: 'Conta #01', value: 11.15, date: DateTime.now()),
-    Transaction(
-        id: 't4', title: 'Conta #02', value: 201.15, date: DateTime.now()),
-    Transaction(id: 't5', title: 'Conta #03', value: 15, date: DateTime.now()),
-    Transaction(
-        id: 't6', title: 'Conta #04', value: 18.90, date: DateTime.now()),
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
   ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+    Navigator.of(context).pop();
+  }
 
   _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
@@ -55,51 +89,34 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  _addTransaction(String title, double value) {
-    final newTransaction = Transaction(
-        id: Random().nextDouble().toString(),
-        title: title,
-        value: value,
-        date: DateTime.now());
-
-    setState(() {
-      _transactions.add(newTransaction);
-    });
- 
-    Navigator.of(context).pop();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
-        // BOTAO DE ADD DESPESA PESSOAL (SUPERIOR)
         actions: <Widget>[
           IconButton(
-            onPressed: () => _openTransactionFormModal(context),
             icon: const Icon(Icons.add),
-          )
+            onPressed: () => _openTransactionFormModal(context),
+          ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            SizedBox(
+            const SizedBox(
               width: double.infinity,
               child: Card(
-                color: Colors.blue[500],
+                color: Colors.blue,
                 elevation: 5,
-                child: const Text('Grafico...'),
+                child: Text('Gráfico'),
               ),
             ),
             TransactionList(transactions: _transactions),
           ],
         ),
       ),
-
-      // BOTAO DE ADD DESPESA PESSOAL (INFERIOR)
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () => _openTransactionFormModal(context),
