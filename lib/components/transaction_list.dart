3081160f808e79dc8c368833b527b8041aebc2a0
Tupi Guedes <tupiguedes@outlook.com>
2022-12-16 00:47:01 -0300

    Implementação visual: Imagem sem transação - v1.0 - transaction_list

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -16,47 +16,67 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: ListView.builder(
-          itemCount: transactions.length,
-          itemBuilder: (ctx, index) {
-            final tr = transactions[index];
-
-            return Card(
-              child: Row(
+      child: transactions.isEmpty
+          ? Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: Column(
                 children: <Widget>[
-                  // PREÇO
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Theme.of(context).colorScheme.primary,
-                            width: 2)),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}',
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
+                  Text(
+                    'Nenhuma Transação Cadastrada!',
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
-                  // TITULO E DATA
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tr.title,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Text(
-                        DateFormat('d MMM y')
-                            .format(tr.date), // '${tr.date.toString()}',
-                        style: const TextStyle(color: Colors.grey),
-                      )
-                    ],
+                  Container(
+                    margin: const EdgeInsets.only(top: 40),
+                    height: 200,
+                    child: Image.asset(
+                      'assets\\images\\waiting.png',
+                      fit: BoxFit.cover,
+                    )
                   )
                 ],
               ),
-            );
-          }),
+          )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final tr = transactions[index];
+
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      // PREÇO
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).colorScheme.primary,
+                                width: 2)),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                      ),
+                      // TITULO E DATA
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tr.title,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          Text(
+                            DateFormat('d MMM y')
+                                .format(tr.date), // '${tr.date.toString()}',
+                            style: const TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }),
     );
   }
 }
